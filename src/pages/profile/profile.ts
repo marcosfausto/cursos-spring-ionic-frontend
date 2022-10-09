33dd9c3cf938d7c984c2d613413551258e4a9dee
Marcos <marcos_fausto@yahoo.com.br>
2022-10-09 18:40:29 +0100

    Mostrando dados e imagem do cliente para pagina de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.services';
 
 /**
@@ -17,17 +20,32 @@ import { StorageService } from '../../services/storage.services';
 export class ProfilePage {
 
   email: string;
+  cliente: ClienteDTO;
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     public storage: StorageService) {
+     public storage: StorageService,
+     public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let LocalUser = this.storage.getLocalUser();
     if (LocalUser && LocalUser.email) { // checar se é nulo
-      this.email = LocalUser.email;
+      this.clienteService.findByEmail(LocalUser.email)
+      .subscribe(response => {
+        this.cliente = response;
+        this.getImageIfExists();
+      },
+      error => {});
     }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+    },
+    error => {});
   }
 
 }
