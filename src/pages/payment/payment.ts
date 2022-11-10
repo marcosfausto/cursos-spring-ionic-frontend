d2d1c7ef5e31c658efdddac88df39516de05096f
Marcos <marcos_fausto@yahoo.com.br>
2022-11-10 21:44:01 +0000

    Tela de confirmacao de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PedidoDTO } from '../../models/pedido.dto';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -12,26 +12,25 @@ export class PaymentPage {
 
   pedido: PedidoDTO;
 
-  parcelas: number[] = [1,2,3,4,5,6,7,8,9,10]
+  parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   formGroup: FormGroup;
 
   constructor(
-    public navCtrl: NavController,
+    public navCtrl: NavController, 
     public navParams: NavParams,
     public formBuilder: FormBuilder) {
 
-      this.pedido = this.navParams.get('pedido');
+    this.pedido = this.navParams.get('pedido');
 
-      this.formGroup = this.formBuilder.group({
-        numeroDeParcelas : [1,Validators.required],
-        "@type": ["pagamentoComCartao", Validators.required]
-      });
+    this.formGroup = this.formBuilder.group({
+      numeroDeParcelas: [1, Validators.required],
+      "@type": ["pagamentoComCartao", Validators.required]
+    });
   }
 
   nextPage() {
     this.pedido.pagamento = this.formGroup.value;
-    console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
   }
-
 }
